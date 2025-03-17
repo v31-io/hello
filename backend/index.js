e2c0e3a47ca5bfc85d0e235ac382e8218a1d65f8
sidxdev <sidsriv93@gmail.com>
2025-03-17 08:32:59 +0000

    init

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+import morgan from 'morgan';
+import cors from 'cors';
+import express from 'express';
+import { createClient } from '@redis/client';
+
+// Check redis connection
+const redisCredentials = {
+  username: process.env.REDIS_USER,
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST
+  }
+}
+
+const client = await createClient(redisCredentials)
+  .on('error', err => console.log('Redis Client Error', err))
+  .connect();
+
+await client.lPush('hello:hello', Date.now().toString());
+await client.disconnect();
+
+const app = express()
+
+app.use(cors())
+app.use(morgan('combined'))
+
+app.get('/api', (req, res) => {
+  res.json([
+    {
+      "id":"1",
+      "title":"Book Review: The Name of the Wind"
+    },
+    {
+      "id":"2",
+      "title":"Game Review: Pokemon Brillian Diamond"
+    },
+    {
+      "id":"3",
+      "title":"Show Review: Alice in Borderland"
+    }
+  ])
+})
+
+app.listen(4000, () => {
+  console.log('listening for requests on port 4000')
+})
